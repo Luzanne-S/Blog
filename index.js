40717ccfd6520e99ff29889543151018de6a5838
Luzanne <86235334+Luzanne-S@users.noreply.github.com>
2021-10-27 15:40:06 +0200

    Updated index.js
    Removed unused code
    changed alert to change form inner html

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let id = (id) => document.getElementById(id);
-let name = id("name");
-let email = id("email");
-let form = id("contactForm");
+
 
 function handle_form_submission() {
-  alert("Thank you, I will contact you soon");
+  const contactForm = document.getElementById("contactForm");
+
+  contactForm.innerHTML = "<h2>Thank you, I will contact you soon</h2>";
   return false;
 }
